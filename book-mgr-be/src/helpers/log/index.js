a79bdcfd9b6b9e0ca78b5beb33257e2da81d5d4d
Gary <garymur301@gmail.com>
2024-04-06 23:59:27 +0800

    feat:支持日志的隐藏操作

--- book-mgr-be/src/helpers/log/index.js
+++ book-mgr-be/src/helpers/log/index.js
@@ -4,7 +4,24 @@ const mongoose = require('mongoose');
 const Log = mongoose.model('Log');
 const LogResponse = mongoose.model('LogResponse');
 
+const excludedUrlPatterns = [
+  /^\/user\/info/,
+  /^\/character\/list/,
+  /^\/log\/list/,
+  /^\/log\/delete/,
+  /^\/book\/list/,
+  /^\/user\/list/,
+]; // Add more patterns to exclude if needed
+
 const logMiddleware = async (ctx, next) => {
+  // Check if the request should be excluded from logging
+  for (const pattern of excludedUrlPatterns) {
+    if (pattern.test(ctx.url)) {
+      await next(); // Skip logging and move to the next middleware
+      return; // Exit the middleware
+    }
+  }
+
   const startTime = Date.now();
 
   await next();
@@ -22,11 +39,6 @@ const logMiddleware = async (ctx, next) => {
   const url = ctx.url;
   const method = ctx.method;
   const status = ctx.status;
-  let show = true;
-
-  if (url === '/log/delete') {
-    show = false;
-  }
 
   let responseBody = '';
 
@@ -44,25 +56,21 @@ const logMiddleware = async (ctx, next) => {
 
   const log = new Log({
     user: payload.account,
-
     request: {
       url,
       method,
       status,
     },
-
     endTime,
     startTime,
-    show,
+    show: true,
   });
-
   log.save();
 
   const logRes = new LogResponse({
     logId: log._id,
     data: responseBody,
   });
-
   logRes.save();
 };
 
